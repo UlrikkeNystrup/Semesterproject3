3f19a70af5ef65816db26f8ab0f36ab61c85625e
ulrikkenystrup <s215849@dtu.dk>
2022-12-02 15:06:04 +0100

    Tilføjet kommentar i SqlConnection klassen

--- src/main/java/Data/SqlConnection.java
+++ src/main/java/Data/SqlConnection.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlConnection {
-
-
     //Create a Connection object
     private static Connection connection;
 
@@ -14,11 +12,12 @@ public class SqlConnection {
     public static Connection getConnection(){
         /* metode typen er Connection, dermed skal metoden returnere et connection objekt (ligesom hvis det var en int eller double)
         klassemetode, derfor man bruger nøgleordet Static
+        --> man bruger nøgleordet Static, fordi det er en klassemetode.
+        Man opretter et Connection objekt inde i den klasse hvor man vil bruge forbindelsen (RegistrerKonsServlet), og så kalder man getConnection() metoden på det.
          */
         try {
             if (connection==null || connection.isClosed()) {
                 connection = DriverManager.getConnection("jdbc:mysql://db.caprover.diplomportal.dk/s215849?"
-
                         + "user=s215849&password=VZQM7OAP0CnLXTZ4rqYgw");
             }
         } catch (SQLException e) {
@@ -27,4 +26,4 @@ public class SqlConnection {
         return connection;
     }
 }
-//kommentar
+

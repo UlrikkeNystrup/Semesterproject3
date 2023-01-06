4938f10bd26afbb12e304a94ffd6ff97fc5b757e
juliane <s215843@student.dtu.dk>
2023-01-06 11:41:50 +0100

    ny

--- src/main/java/DataAccessLayer/EkgDTO.java
+++ src/main/java/DataAccessLayer/EkgDTO.java
@@ -1,6 +1,35 @@
 package DataAccessLayer;
 
+import java.sql.Timestamp;
+
 public class EkgDTO {
 
+    private String cpr;
+    private Timestamp tid;
+    private double spænding;
 
+    public String getCpr() {
+        return cpr;
+    }
+    public Timestamp getTid() {
+        return tid;
+    }
+    public double getSpænding() {
+        return spænding;
+    }
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+    public void setTid(Timestamp tid) {
+        this.tid = tid;
+    }
+    public void setSpænding(double spænding) {
+        this.spænding = spænding;
+    }
+
+    public String toString(){
+        return "EkgDTO{"+
+                "cpr='"+cpr + '\'+
+
+    }
 }
